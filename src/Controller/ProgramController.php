084c012282400ea8c5d4b7afd27a77c952672be7
Yann Raulet <yann.raulet@hotmail.fr>
2020-12-08 18:22:28 +0100

    Symfony quest 9

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Program;
+use App\Entity\season;
+use App\Repository\SeasonRepository;
+
+
 
 /**
  * @Route("/programs", name="program_")
@@ -14,9 +18,10 @@ Class ProgramController extends AbstractController
 {
     /**
      * Show all rows from Program's entity
-     * 
-     * @Route("/", name="index")
-     * @return Response A response instance
+     * ns de récupérer en contexte à ta vue program/show.html.twig.
+     * @Route("/", name="index")        $seasonRepository->
+
+     * @return Response AThis controller must have an optionnal name parameter (in the URL path).
      */
     public function index(): Response
     {
@@ -32,23 +37,41 @@ Class ProgramController extends AbstractController
 
     /**
      * Getting a program by id
-     *
      * @Route("/show/{id<^[0-9]+$>}", name="show")
      * @return Response
      */
-    public function show(int $id):Response
+    public function show(int $id): Response
     {
         $program = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findOneBy(['id' => $id]);
-
-        if (!$program) {
+        ->getRepository(Program::class)
+        ->findOneBy(['id' => $id]);
+        
+        if(!$program) {
             throw $this->createNotFoundException(
-                'No program with id : '.$id.' found in program\'s table.'
+                'No program with id : ' .$id . 'found in program\'s table.'
             );
         }
+        
         return $this->render('program/show.html.twig', [
-            'program' => $program,
+                'program' => $program
+            ]);
+    }
+
+    /**
+     * Show season of a program
+     * @Route("/{programId}/season/{seasonId}", name="season_show")
+     */
+    public function showSeason(int $programId, int $seasonId, SeasonRepository $seasonRepository): Response
+    {
+        $season = $this->getDoctrine()
+        ->getRepository(Season::class)
+        ->find(['id' => $seasonId]);
+        
+
+        return $this->render('program/season_show.html.twig', [
+            'programId' => $programId,
+            'seasonId' => $seasonId,
+            'season' => $season
         ]);
     }
 }
